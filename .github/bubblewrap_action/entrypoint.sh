c47a7fa7e79e1f4cd83220181e8a4b9ff232301e
axel paulander <axel.paulander@gmail.com>
2024-05-19 17:49:52 +0200

    Check workflow again

--- .github/bubblewrap_action/entrypoint.sh
+++ .github/bubblewrap_action/entrypoint.sh
@@ -2,8 +2,7 @@
 
 echo "========================= Changing directory to $1 ========================="
 cd $1
-ls -l
 echo "========================= Building APK, it may take 3-4 minutes or more ========================="
-( sleep 5 && while [ 1 ]; do sleep 1; echo y; done ) | bubblewrap build --skipSigning
+( sleep 5 && while [ 1 ]; do sleep 5; echo y; done ) | bubblewrap build --skipSigning
 cp ./*.apk ..
 echo "========================= APK building finished ========================="
